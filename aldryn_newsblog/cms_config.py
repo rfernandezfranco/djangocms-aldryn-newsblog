180a06e1225a5c637675da4c615ce0ec7d66e972
rfernandezfranco <101652178+rfernandezfranco@users.noreply.github.com>
2025-06-15 02:33:27 -0300

    Fix versioning config for Article translations

---
+++ aldryn_newsblog/cms_config.py
@@ -0,0 +1,17 @@
+from cms.app_base import CMSAppConfig
+
+from djangocms_versioning.datastructures import VersionableItem, default_copy
+
+from .models import Article
+
+
+class NewsBlogVersioningConfig(CMSAppConfig):
+    djangocms_versioning_enabled = True
+    versioning = [
+        VersionableItem(
+            content_model=Article._parler_meta.root_model,
+            grouper_field_name="master",
+            extra_grouping_fields=["language_code"],
+            copy_function=default_copy,
+        )
+    ]
